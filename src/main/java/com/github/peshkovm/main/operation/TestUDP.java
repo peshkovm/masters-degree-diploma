ca3cd3e3cb4ee431fe4b8af86bdcc6830d18ac5d
Maksim Peshkov <peshmaxan@gmail.com>
2021-04-15 18:52:38 +0300

    Add TestUDP

---
+++ src/main/java/com/github/peshkovm/main/operation/TestUDP.java
@@ -0,0 +1,134 @@
+package com.github.peshkovm.main.operation;
+
+import com.github.peshkovm.crdt.CrdtService;
+import com.github.peshkovm.crdt.operation.AbstractCmRDT;
+import com.github.peshkovm.crdt.operation.GCounterCmRDT;
+import com.github.peshkovm.crdt.operation.protocol.DownstreamUpdate;
+import com.github.peshkovm.crdt.routing.ResourceType;
+import com.github.peshkovm.main.common.TestUtils;
+import com.github.peshkovm.node.InternalNode;
+import com.github.peshkovm.raft.discovery.ClusterDiscovery;
+import com.github.peshkovm.transport.DiscoveryNode;
+import com.github.peshkovm.transport.TransportService;
+import com.github.peshkovm.transport.netty.NettyTransportService;
+import io.vavr.collection.Vector;
+import io.vavr.control.Option;
+import java.util.concurrent.TimeUnit;
+
+public class TestUDP extends TestUtils {
+
+  private Vector<CrdtService> crdtServices;
+  private TransportService transportService;
+
+  public static void main(String[] args) throws Exception {
+    final TestUDP testInstance = new TestUDP();
+    try {
+      testInstance.setUpNodes();
+      testInstance.shouldNotConvergeWhenUsingUdpProtocol();
+    } finally {
+      testInstance.tearDownNodes();
+    }
+  }
+
+  void shouldNotConvergeWhenUsingUdpProtocol() throws Exception {
+    final String crdtId = "countOfLikes";
+    final int timesToIncrement = 100;
+    final long numOfSecondsToWait = TimeUnit.SECONDS.toMillis(2);
+
+    createResource(crdtId, ResourceType.GCounterCmRDT);
+
+    final Vector<GCounterCmRDT> gCounters =
+        crdtServices
+            .map(CrdtService::crdtRegistry)
+            .map(crdtRegistry -> crdtRegistry.crdt(crdtId, GCounterCmRDT.class));
+
+    for (int incrementNum = 0; incrementNum < timesToIncrement; incrementNum++) {
+      final GCounterCmRDT sourceGCounter = gCounters.head();
+      sourceGCounter.increment();
+
+      if (incrementNum == timesToIncrement / 2) {
+        final Class<AbstractCmRDT<Long, Long>> type =
+            (Class<AbstractCmRDT<Long, Long>>) ((AbstractCmRDT) sourceGCounter).getClass();
+
+        final DownstreamUpdate<Long, Long> downstreamUpdate =
+            new DownstreamUpdate<>(crdtId, Option.none(), type, 1L);
+
+        final DiscoveryNode nodeToSend =
+            nodes.get(1).getBeanFactory().getBean(ClusterDiscovery.class).getSelf();
+
+        transportService.send(nodeToSend, downstreamUpdate);
+      }
+    }
+
+    logger.info("Waiting for query");
+    for (int i = 0; i < numOfSecondsToWait / 100; i++) {
+      if (!gCounters
+          .zipWithIndex()
+          .forAll(
+              tuple2 -> {
+                final GCounterCmRDT counter = tuple2._1();
+                final Integer index = tuple2._2();
+
+                return index == 1
+                    ? counter.query() == timesToIncrement + 1
+                    : counter.query() == timesToIncrement;
+              })) {
+        TimeUnit.MILLISECONDS.sleep(100);
+      } else {
+        break;
+      }
+    }
+
+    gCounters
+        .zipWithIndex()
+        .forEach(
+            tuple2 -> {
+              final GCounterCmRDT counter = tuple2._1();
+              final Integer index = tuple2._2();
+
+              if (!(index == 1
+                  ? counter.query() == timesToIncrement + 1
+                  : counter.query() == timesToIncrement)) {
+                logger.info("FAILURE");
+                throw new AssertionError(
+                    "\nExpected :" + timesToIncrement + "\nActual   :" + counter.query());
+              }
+            });
+    logger.info("SUCCESSFUL");
+  }
+
+  void setUpNodes() {
+    createAndStartInternalNode();
+    createAndStartInternalNode();
+    createAndStartInternalNode();
+
+    connectAllNodes();
+
+    crdtServices = nodes.map(node -> node.getBeanFactory().getBean(CrdtService.class));
+    transportService = nodes.head().getBeanFactory().getBean(TransportService.class);
+  }
+
+  private void createResource(String crdt, ResourceType crdtType) {
+    crdtServices.head().addResource(crdt, crdtType).get();
+  }
+
+  private void partition(InternalNode nodeToDisconnectFrom) {
+    final InternalNode sourceNode = nodes.head();
+
+    final NettyTransportService sourceTransportService =
+        sourceNode.getBeanFactory().getBean(NettyTransportService.class);
+
+    sourceTransportService.disconnectFromNode(
+        nodeToDisconnectFrom.getBeanFactory().getBean(ClusterDiscovery.class).getSelf());
+  }
+
+  private void recoverFromPartition(InternalNode nodeToConnectTo) {
+    final InternalNode sourceNode = nodes.head();
+
+    final NettyTransportService sourceTransportService =
+        sourceNode.getBeanFactory().getBean(NettyTransportService.class);
+
+    sourceTransportService.connectToNode(
+        nodeToConnectTo.getBeanFactory().getBean(ClusterDiscovery.class).getSelf());
+  }
+}
